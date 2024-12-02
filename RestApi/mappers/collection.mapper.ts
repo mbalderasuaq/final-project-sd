c7a5cffffc8db9f6b0020c0bc9f705b716cfd48e
Miguel Angel Balderas Balderas <mbalderas08@alumnos.uaq.mx>
2024-12-01 22:07:32 -0600

    feat: add mappers

---
+++ RestApi/mappers/collection.mapper.ts
@@ -0,0 +1,11 @@
+import type { CollectionEntity } from "../infrastructure/mongo/collection.entity";
+import type { CollectionModel}  from "../models/collection.model";
+
+export function CollectionEntityToModel(taskEntity: CollectionEntity): CollectionModel {
+    return {
+        id: taskEntity.id,
+        title: taskEntity.title,
+        description: taskEntity.description,
+        tasks: taskEntity.tasks
+    };
+}
